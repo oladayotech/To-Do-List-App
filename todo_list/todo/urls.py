2126fbccfa1f25af904512a83ee7f1aa5ba6a966
oladayotech <d1arogundade@gmail.com>
2025-03-29 03:04:58 +0100

    third commit

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, TaskList
+from .views import home, TaskList, TaskDetail
 
 urlpatterns = [
     path('', home, name='home'),
     path('tasks/',  TaskList.as_view(), name='tasks'),
+    path('tasks/<int:pk>/',  TaskDetail.as_view(), name='task'),
 ]
