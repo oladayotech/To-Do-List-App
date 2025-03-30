c3bb5d4ed73ea2718cdef197c8ab1c15a6989c7d
oladayotech <d1arogundade@gmail.com>
2025-03-30 02:14:01 +0100

    fourth maybe fifht commit not counting lol

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib import messages
+from django.urls import reverse_lazy
 from .models import Task
 
 # Create your views here.
@@ -14,3 +17,33 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
+    
+class TaskCreate(CreateView):
+    model = Task
+    fields = ['title', 'description', 'completed']
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, "The task was sucessfully.")
+        return super(TaskCreate, self).form_valid(form)
+    
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['title', 'description', 'completed']
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'The task was updated successfully.')
+        return super(TaskUpdate,self).form_valid(form)
+    
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'The task was deleted successfully.')
+        return super(TaskDelete,self).form_valid(form)
