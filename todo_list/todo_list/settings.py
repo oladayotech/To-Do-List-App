a0998b9c0e3b5be2e7636a7e6333385bbe1660ea
oladayotech <d1arogundade@gmail.com>
2025-04-01 02:43:11 +0100

    Seventh commit loving it

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -126,3 +126,14 @@ STATICFILES = [BASE_DIR / 'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PORT = 25
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_gmail_account@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_gmail_password'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
--- todo_list/users/urls.py
+++ todo_list/users/urls.py
@@ -10,7 +10,40 @@ from django.contrib.auth.views import (
 from .views import MyLoginView, RegisterView
 
 urlpatterns = [
-    path('login/', MyLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', 
+         MyLoginView.as_view(), 
+         name='login'
+    ),
+    
+    path('logout/', 
+         LogoutView.as_view(next_page='login'), 
+         name='logout'
+    ),
+    
+    path('register/', 
+         RegisterView.as_view(), 
+         name='register'
+    ),
+    
+    path('password-reset/', 
+         PasswordResetView.as_view(template_name='users/password_reset.html',
+         html_email_template_name = 'users/password_reset_email.html'
+         ), 
+         name='password_reset'
+    ),
+    
+    path('password-reset/done/', 
+         PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), 
+         name='password_reset_done'
+    ),
+    
+    path('password-reset-confirm/<uidb64>/<token>/', 
+         PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), 
+         name='password_reset_confirm'
+    ),
+    
+    path('password-reset-complete/', 
+         PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), 
+         name='password_reset_complete'
+    ),
 ]
